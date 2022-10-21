44e9157d6b4ef2fa73aac962598d0f7edc3f033a
junaid bashir <89861553+Junaid-Bashir-Kernel@users.noreply.github.com>
2022-10-21 15:32:53 +0530

    added moralis

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
+//Moralis
+import { createClient, configureChains, defaultChains, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { SessionProvider } from 'next-auth/react';
+const { provider, webSocketProvider } = configureChains(defaultChains, [publicProvider()]);
+
+//Moralis
+
+const client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: true,
+});
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+  return  (
+    
+    <WagmiConfig client={client}>
+    <SessionProvider session={pageProps.session} refetchInterval={0}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  </WagmiConfig>
+
+
+  )
 }
 
 export default MyApp

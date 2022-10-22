9756afad434786a7d66455107a07de0fc5395a55
junaid bashir <89861553+Junaid-Bashir-Kernel@users.noreply.github.com>
2022-10-23 03:29:12 +0530

    added cookies from front

--- components/Fitbit.js
+++ components/Fitbit.js
@@ -1,7 +1,26 @@
 import DynamicPlot from "../components/Plot"
 import Link from "next/link"
+import { getCookies} from 'cookies-next';
+import { useState } from "react";
+
 export default function Fitbit(){
 
+const [res,setRes]=useState("")
+
+async function getFitbitData() {
+     
+let request=await fetch("https://api.fitbit.com/1/user/-/activities/goals/daily.json",{
+     method:"get",
+     headers:{
+"Accept": "application/json",
+"Authorization": `Bearer ${getCookies("fittoken")}`
+     }
+})
+ let response=await request.json()
+ console.log(response)
+ setRes(response)
+ console.log(res)
+ }    
 
 return (
  <div className="container">
@@ -18,9 +37,13 @@ return (
 <button className="btn btn-dark"><Link href="/api/getcodefitbit" style={{color:"white",textDecoration:"none"}}>Fitbit Connect</Link></button>
      </td>
      <td>
-      <button className="btn btn-danger">get Steps</button>
+      <button className="btn btn-danger" onClick={()=>getFitbitData()}>get Goals</button>
      </td>
      <td>
+        <div className="container">
+          
+          <pre  className="container bg-dark" style={{color:"white"}}>{JSON.stringify(res,null,2)}</pre>
+          </div>  
       
      <DynamicPlot/>
 
